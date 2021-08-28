8f13b4fdebb8bcce74c004b53aea0e382305ca40
zakino123 <15rb076@ms.dendai.ac.jp>
2021-08-28 18:10:36 +0900

    ユーザー情報変更機能実装 #24

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   def User.new_token
     SecureRandom.urlself_base64
