fe1b6e4adf17c01cf3adeb69c4cd0df94ecb7625
zakino123 <15rb076@ms.dendai.ac.jp>
2021-08-31 11:52:33 +0900

    editアクション・フレンドリーフォワーディングのテスト構築#28

---
+++ spec/features/users_edit_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.feature "Edit", type: :feature do
+  let(:user) { FactoryBot.create(:user) }
+  describe "ユーザー情報の変更" do
+    before do
+      visit user_create_path
+      fill_in 'ニックネーム', with: 'testuser'
+      fill_in 'メールアドレス', with: 'testuser@example.com'
+      fill_in 'パスワード', with: 'password'
+      fill_in 'パスワード(確認用)', with: 'password'
+      click_button 'ユーザー作成'
+    end
+    it "編集に成功" do
+      click_link "ユーザー情報変更"
+      fill_in "ニックネーム", with: "test"
+      click_button "変更"
+
+      expect(user.reload.name).to eq "test"
+    end
+    it "編集に失敗" do
+      click_link "ユーザー情報変更"
+      fill_in "メールアドレス", with: "test@invalid"
+      click_button "変更"
+
+      expect(user.reload.email).to_not eq "test@invalid"
+    end
+
+  end
+end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -7,5 +7,39 @@ RSpec.describe "Users", type: :request do
       expect(response).to have_http_status(200)
     end
   end
-
+  describe "editアクション" do
+    before do
+      @user = FactoryBot.create(:user)
+      @other_user = FactoryBot.create(:user)
+    end
+    context "正しいユーザーがログインした場合(フレンドリーフォワーディングも確認)" do
+      before do
+        get edit_user_path(@user)
+        session_params = { session: { email: @user.email, password: @user.password } }
+        post login_path, params: session_params
+      end
+      it "edit画面表示の成功" do
+        expect(response).to have_http_status "302"
+        expect(response).to redirect_to edit_user_path(@user)
+      end
+    end
+    context "正しいユーザーがログインしていない場合" do 
+      it "ログインページにリダイレクト" do
+        get edit_user_path(@user)
+        expect(response).to have_http_status "302"
+        expect(response).to redirect_to login_path
+      end
+    end
+    context "違うアカウントの場合" do 
+      before do
+        session_params = { session: { email: @other_user.email, password: @other_user.password } }
+        post login_path, params: session_params
+      end
+      it "ログインページにリダイレクト" do
+        get edit_user_path(@user)
+        expect(response).to have_http_status "302"
+        expect(response).to redirect_to root_path
+      end
+    end
+  end
 end
