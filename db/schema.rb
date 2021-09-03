60c6e420e339cb16119c046c72a8f2ba9a05f427
zakino123 <15rb076@ms.dendai.ac.jp>
2021-09-03 11:40:47 +0900

    db-edit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_28_015300) do
+ActiveRecord::Schema.define(version: 2021_09_03_013246) do
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,10 @@ ActiveRecord::Schema.define(version: 2021_08_28_015300) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name:  "管理人ユーザー",
+  email: "admin@example.com",
+  password:              "foobar",
+  password_confirmation: "foobar",
+  admin:     true,
+  activated: true,
+  activated_at: Time.zone.now)
