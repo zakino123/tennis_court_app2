9a3e1e17916da502a35e48df4761a64842030296
zakino123 <15rb076@ms.dendai.ac.jp>
2021-09-07 16:56:02 +0900

    search-form#60

--- app/models/court.rb
+++ app/models/court.rb
@@ -1,4 +1,7 @@
 class Court < ApplicationRecord
+  reverse_geocoded_by :latitude, :longitude
+  after_validation :reverse_geocode
+  after_validation :geocode
   belongs_to :user
   mount_uploader :image, ImageUploader
   # validates :user_id, presence: true
@@ -10,4 +13,21 @@ class Court < ApplicationRecord
   validates :latitude, presence: true
   validates :longitude, presence: true
   validates :remarks, length: { maximum: 200 }
+
+  class << self
+    def within_box(distance, latitude, longitude)
+      distance = distance
+      center_point = [latitude, longitude]
+      box = Geocoder::Calculations.bounding_box(center_point, distance)
+      self.within_bounding_box(box)
+    end
+
+    def geocode
+      uri = URI.escape("https://maps.googleapis.com/maps/api/geocode/json?address="+self.address.gsub(" ", "")+"&key=[ここにAPIキー]")
+      res = HTTP.get(uri).to_s
+      response = JSON.parse(res)
+      self.latitude = response["results"][0]["geometry"]["location"]["lat"]
+      self.longitude = response["results"][0]["geometry"]["location"]["lng"]
+    end
+  end
 end
