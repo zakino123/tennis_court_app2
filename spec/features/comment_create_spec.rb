54a68701523a93a806b7c095c57569bfa2975da4
zakino123 <15rb076@ms.dendai.ac.jp>
2021-09-08 20:01:14 +0900

    comment-test#69

---
+++ spec/features/comment_create_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.feature "New", type: :feature do
+  let(:user) { FactoryBot.create(:user) }
+  let(:court) { FactoryBot.create(:court) }
+  describe "コートコメントの作成" do
+    before do
+      visit user_create_path
+      fill_in 'ニックネーム', with: 'testuser'
+      fill_in 'メールアドレス', with: 'testuser@example.com'
+      fill_in 'パスワード', with: 'password'
+      fill_in 'パスワード(確認用)', with: 'password'
+      click_button 'ユーザー作成'
+      visit court_path(court)
+    end
+    context "正しい値を入力した場合" do
+      before do
+        fill_in "comment_context", with: "test"
+        click_button "コメントする"
+      end
+      it "作成に成功 " do
+        expect(page).to have_selector('.alert-success', text: 'コメントを投稿しました！')
+      end
+    end
+    context "誤った値を入力した場合" do
+      before do
+        fill_in "comment_context", with: ""
+        click_button "コメントする"
+      end
+      subject { page }
+      it "作成に失敗" do
+        is_expected.to have_css('.alert-danger', text: '投稿に失敗しました！')
+      end
+    end
+  end
+end
