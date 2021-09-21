4dfbed53d5ab4b6224fb838413cf62d65f079697
zakino123 <15rb076@ms.dendai.ac.jp>
2021-09-21 14:32:21 +0900

    package-lock.json-delete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,8 @@
-const colors = require('tailwindcss/colors');
-
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      colors: {
-        'teal': colors.teal
-      }
     },
   },
   variants: {
