19a26b3a26673aeeacac0232ecbd46cd0daedc61
zakino123 <15rb076@ms.dendai.ac.jp>
2021-09-21 20:03:31 +0900

    application.js-change

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
 
 import 'bootstrap';
 import '@fortawesome/fontawesome-free/js/all';
@@ -15,6 +15,6 @@ import '../css/tailwind.css';
 
 require("@fortawesome/fontawesome-free");
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
