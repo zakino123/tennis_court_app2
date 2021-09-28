7953cfcdcc114f693a86163a8dffcf7e7b8fb407
zakino123 <15rb076@ms.dendai.ac.jp>
2021-09-28 10:16:16 +0900

    tailwindcss purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: [],
+  purge: ["./app/**/*.html.erb", "./app/**/*.css", "./app/**/*.scss", "./app/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
