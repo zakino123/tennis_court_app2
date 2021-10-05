28c96ade36e9d2ae9fa5d478a0297bc2da1721a7
zakino123 <15rb076@ms.dendai.ac.jp>
2021-10-05 10:55:25 +0900

    carrierwave-new2

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,4 +1,8 @@
 if Rails.env.production?
+  require 'carrierwave/storage/abstract'
+  require 'carrierwave/storage/file'
+  require 'carrierwave/storage/fog'
+
   CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
