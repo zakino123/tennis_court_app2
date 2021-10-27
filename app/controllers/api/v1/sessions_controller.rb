85227b15dee5dec5b19d9297fdd7e65a09ebd8ef
zakino123 <15rb076@ms.dendai.ac.jp>
2021-10-27 11:21:01 +0900

    セッションAPIコントローラー#104

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::SessionsController < ApiController
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
+      log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_back_or user
+    else
+      flash.now[:danger] = 'メールアドレスかパスワードが間違っています。'
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
+  end
+end
